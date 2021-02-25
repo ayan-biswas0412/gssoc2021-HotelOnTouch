12c5a01635aaf5b023f0086d1aa684b0fb8bdbcd
HRITHIK GOSWAMI <51878340+batman005@users.noreply.github.com>
2021-02-25 11:46:37 +0530

    Update App.js

--- hotelontouch/src/App.js
+++ hotelontouch/src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { MemoryRouter, Switch, Route } from 'react-router-dom';
 import { HomePage } from './pages';
 import { NavbarComponent } from './components';
-import { Footer } from './components';
+import Footer from './components/Footer';
 import './App.css';
 const App = () => (
   <MemoryRouter>
-      <NavbarComponent/>
-      <Switch>
-        <Route path="/" component={HomePage}></Route>
-      </Switch>
-      <Footer/>
-     </MemoryRouter> 
+   <NavbarComponent/>
+    <Switch>
+    <Route path="/" component={HomePage}></Route>
+    </Switch>
+    <Footer/>
+   </MemoryRouter> 
 );
 export default App;
