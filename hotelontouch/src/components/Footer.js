2350d5295d15ac13e60c350c21b0196981d7ce9b
HRITHIK GOSWAMI <51878340+batman005@users.noreply.github.com>
2021-02-25 11:02:32 +0530

    Update Footer.js
    now it will work for all devices at 100% zoom ratio

--- hotelontouch/src/components/Footer.js
+++ hotelontouch/src/components/Footer.js
@@ -12,18 +12,16 @@ const Container=styled.div`
   display:flex;
   flex-wrap:wrap;
   justify-content:space-around;
-  
   align-items:center;
-  height :350px; 
-  padding-bottom:20px;  
-  
+  padding-bottom:290px;
+  width: 100%; 
+  height:700px;
 `
 const Quick=styled.div`
 `
 const About=styled.div`
 @media(max-width:500px){
-    margin-left:10px;
-  
+    margin-left:10px;  
 }
 `
 const Contact=styled.div`
@@ -31,47 +29,41 @@ const Contact=styled.div`
 const Body=styled.body`
 display:flex;
 flex-direction:column;
-
-
 `
 function Footer(){
     return(
-
 <Body>
 <Wave fill='#1089ff'
         paused={false}
         options={{
-          height: 20,
-          amplitude: 20,
+          height: 60,
+          amplitude: 25,
           speed: 0.5,
           points: 3
         }}
         style={{position:"relative",
-        bottom:"-44.5%",zIndex:5}}/>
+        bottom:"-24.5%",zIndex:1}}/>
         <Container>
           <About>
-                <h4>HotelOnTouch</h4>
+              <h4>HotelOnTouch</h4>
                 <div className="icons">
                    <a href="https://github.com/ayan-biswas0412/gssoc2021-HotelOnTouch" style={{marginRight:'5px'}}><FontAwesomeIcon icon={faGithub} size='2x' color ='white'/></a>
-
                 </div>
                 </About>
            <Quick>
                <h4>Quick Links</h4>
-                <a href="#" style={{color:"#fff"}}><li style={{listStyle:'none'}}>Privacy Policy</li></a>
-                <a href="#"  style={{color:"#fff"}}><li style={{listStyle:'none'}}>Terms & Conditions</li></a>
-                <a href="#"  style={{color:"#fff"}}><li style={{listStyle:'none'}}>Disclaimer</li></a>
+                <a href="/" style={{color:"#fff"}}><li style={{listStyle:'none'}}>Privacy Policy</li></a>
+                <a href="/"  style={{color:"#fff"}}><li style={{listStyle:'none'}}>Terms & Conditions</li></a>
+                <a href="/"  style={{color:"#fff"}}><li style={{listStyle:'none'}}>Disclaimer</li></a>
             </Quick>
           <Contact>
                 <h4>Contact Us</h4>
-                 <a href="#" style={{color:"#fff"}}><li style={{listStyle:'none'}}><CallIcon/>12345689</li></a>
-                 <a href="#"  style={{color:"#fff"}}><li style={{listStyle:'none'}}><MailIcon/>HotelOnTouch@gmail.com</li></a>
-                <a href="#"  style={{color:"#fff"}}><li style={{listStyle:'none'}}><PublicIcon/>HotelOnTouch.com</li></a>
-
-                </Contact>
+                 <a href="/" style={{color:"#fff"}}><li style={{listStyle:'none'}}><CallIcon/>12345689</li></a>
+                 <a href="/"  style={{color:"#fff"}}><li style={{listStyle:'none'}}><MailIcon/>HotelOnTouch@gmail.com</li></a>
+                <a href="/"  style={{color:"#fff"}}><li style={{listStyle:'none'}}><PublicIcon/>HotelOnTouch.com</li></a>
+        </Contact>
     </Container>
     </Body>
-
-    );
+ );
 }
 export default Footer; 
