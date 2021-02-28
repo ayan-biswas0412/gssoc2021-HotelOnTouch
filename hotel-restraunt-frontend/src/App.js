5fba4116fb8406c70ea1be6441b34a4d6bca1608
vinit-churi <vinitchuri@hotmail.com>
2021-02-28 15:48:45 +0530

    changed default imports to named import

--- hotel-restraunt-frontend/src/App.js
+++ hotel-restraunt-frontend/src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
-import PageOne from "./pages/PageOne";
-import PageTwo from "./pages/PageTwo";
+import { Navbar } from "./components";
+import { HomePage, PageOne, PageTwo } from "./pages";
 function App() {
   return (
     <div className="App">
