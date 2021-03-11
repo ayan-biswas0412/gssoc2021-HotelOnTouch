ca3c7ebd6ef653d0725445623d27f6097bb19548
Doringl <49004028+Doringl@users.noreply.github.com>
2021-03-11 17:18:40 +0300

    conflict

--- hotelontouch/src/components/index.js
+++ hotelontouch/src/components/index.js
@@ -1,2 +1,4 @@
 export { default as NavbarComponent } from './NavbarComponent';
-export { default as ContactUs } from './ContactUs';
+export { default as AdminCard } from './AdminCard';
+export { default as OpenSourceProgramme } from './OpensourceProgramme';
+export { default as AllContributors } from './AllContributors';
