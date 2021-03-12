54e00262a6d4f49da298c898efb07a8a76961ea5
singhayushh <coding.ayush@gmail.com>
2021-03-12 16:38:55 +0530

    user schema for admin, employee and customer type added

---
+++ backend/model/user.model.js
@@ -0,0 +1,143 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+
+// User Schema ... actual users of the application - admin & employees
+const userSchema = new Schema({
+    // Type: admin or employee
+    type: {
+        type: String,
+        default: "employee"
+    },
+    // Full Name
+    name: {
+        type: String,
+        required: true
+    },
+    // Phone Number
+    phone: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    // Email
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    // Password
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    // Store rewarded badges
+    badges: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Badge'
+    }],
+    // On duty or off duty
+    on_duty: {
+        type: Boolean,
+        default: false
+    },
+    // To store if the empoyee is linked to a particular service only e.g.: Chef
+    service_id: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Service'
+    },
+    // User secret to be used in forgot password or account verification
+    user_secret: {
+        type: String,
+        default: null
+    },
+}, {
+    timestamps: true
+});
+
+// Customer Schema ... Customers are handled solely by admins
+const customerSchema = new Schema({
+    // Full Name
+    name: {
+        type: String,
+        required: true
+    },
+    // Phone Number
+    phone: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    // Other room occupants
+    occupancy: {
+        adults: {
+            type: Number,
+            default: 0
+        },
+        children: {
+            type: Number,
+            default: 0
+        }
+    },
+    // Room number
+    room_id: {
+        type: String,
+        default: null
+    },
+    // Type of Room: single, double etc
+    room_type: {
+        type: String,
+        default: null
+    },
+    // Check in date-time
+    check_in: {
+        type: String,
+        default: null
+    },
+    // Check out date-time
+    check_out: {
+        type: String,
+        default: null
+    },
+    // Date-time of booking
+    booking_time: {
+        type: String,
+        required: true
+    },
+    // Active, in-active, booked
+    status: {
+        type: String,
+        required: true
+    },
+    // Amount to be paid
+    balance: {
+        type: String,
+        required: true
+    },
+    // Paymeny Status
+    has_paid: {
+        type: Boolean,
+        default: false
+    },
+}, {
+    timestamps: true
+});
+
+// Hash the password on creation / edit
+userSchema.pre("save", async function (next) {
+    if (!this.isModified || !this.isNew) {
+        next();
+    } else this.isModified("password");
+    if (this.password)
+        this.password = await bcrypt.hash(String(this.password), 12);
+    next();
+});
+
+let Customer = mongoose.model("Customer", customerSchema);
+let User = mongoose.model("User", userSchema);
+
+module.exports = {
+    Customer,
+    User
+}
