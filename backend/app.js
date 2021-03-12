43eec962fa4caaa882241490fb4ea35eef9a7cab
singhayushh <coding.ayush@gmail.com>
2021-03-12 20:18:02 +0530

    Added swagger for api documentation

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,20 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger");
 const app = express();
 const cors = require('cors');
 
 require("dotenv").config();
-
 require("./config/database");
+
+const port = process.env.PORT || 3030;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 require("./route")(app);
-
-const port = process.env.PORT || 3030;
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () =>
   console.log(`Listening on port ${port}...`)
