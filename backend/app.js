cc903c156c0e1e07ee9ecd544f2dd6189e1ff706
singhayushh <coding.ayush@gmail.com>
2021-03-12 16:37:47 +0530

    enabled cross-origin resource sharing

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 require("dotenv").config();
 
 require("./config/database");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 require("./route")(app);
 
 const port = process.env.PORT || 3030;
+
 app.listen(port, () =>
   console.log(`Listening on port ${port}...`)
 );
