b12512e27bfe01ae460049b2071e410dda48cbc6
udaymittal7 <mittaluday435@gmail.com>
2021-03-14 23:57:43 +0530

    removing yarn.lock file from commit

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 const app = express();
 
 require("dotenv").config();
@@ -7,6 +12,23 @@ require("./config/database");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(helmet());
+
+//limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60*60*1000,
+  message: 'To many request from this IP, please try again after an hour!'
+});
+
+app.use('/', limiter);
+
+//data sanitization against noSQL query injection
+app.use(mongoSanitize());
+
+//data sanitization against xss
+app.use(xss());
 
 require("./route")(app);
 
