d2f043c487724f578b1cd82701c7509cc4767f1f
Ankita <ankitapal7777@gmail.com>
2021-03-14 22:59:39 +0530

    enhance we part section

--- hotelontouch/src/components/OpensourceProgramme.js
+++ hotelontouch/src/components/OpensourceProgramme.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Card } from 'react-bootstrap';
-import Wave from 'react-wavify';
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Card, Button } from 'react-bootstrap'
+import Wave from 'react-wavify'
 const Grid = styled.div`
   display: flex;
   flex-direction: row;
@@ -10,24 +10,6 @@ const Grid = styled.div`
   align-items: center;
   text-align: center;
 `;
-
-const Button2 = styled.button`
-  justify-content: center;
-  font-size: 1.3rem;
-  border: 3px solid grey;
-  margin: auto;
-  background-color: orange;
-  border-radius: 6px;
-  margin-top: 3%;
-  box-shadow: 0px 4px 4px #282828;
-  color: #fff;
-
-  &:hover {
-    background-color: black;
-    color: white;
-  }
-`;
-
 const Heading = styled.h1`
   font-size: 4.5rem;
   font-family: "sans-serif";
@@ -39,36 +21,97 @@ const Heading = styled.h1`
   text-shadow: 0px 4px 4px #282828;
 `;
 
-const StyleCard = styled(Card)`
-  width: 20rem;
-  border: 2px solid grey;
-  boxshadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  margin-left: 15%;
-  background-image: url("https://www.transparenttextures.com/patterns/psychedelic.png");
+const CardBody = styled(Card.Body)`
+  background: #0069d9;
+  opacity: 0.9;
+  position: absolute;
+  top: 0%;
+  left: -100%;
+  height: 100%;
+  width: 100%;
+  color: #fff;
+  padding: 20px;
+  display: grid;
+  place-items: center;
+  align-items: center;
+  box-sizing: border-box;
+  transition: all 0.5s;
 `;
-const StyleImg = styled(Card.Img)`
-  margin-top: 3%;
-  border-radius: 50%;
-  width: 50%;
-  border: 4px solid #2c5364;
-  box-shadow: 0px 4px 4px #2c5364;
+
+const Box = styled.div`
+  width: 20rem;
+  height: 24rem;
+  position: relative;
+  margin: 10px auto;
+  overflow: hidden;
+  box-shadow: 0 0 20px 2px rgba(0, 0, 0, 0.5);
+`;
+
+const Image = styled(Card.Img)`
+  width: 70%;
+  height: auto;
+  overflow: hidden;
 `;
 
 const OpenSourceProgramme = () => {
+  const [set, setState] = useState(false);
+
   const cardInfo = [
     {
-      "name": "Girlscript Summer of Code 2021",
-      "url": "https://gssoc.girlscript.tech/index.html",
-      "logo": "https://gssoc.girlscript.tech/images/favicon/favicon.png",
+      name: "Girlscript Summer of Code 2021",
+      url: "https://gssoc.girlscript.tech/index.html",
+      logo: "https://gssoc.girlscript.tech/images/favicon/favicon.png",
     },
   ];
+  function hoverIn() {
+    setState(true);
+  }
+  function hoverOut() {
+    setState(false);
+  }
   const renderCard = (card, index) => {
     return (
       <div>
         <Heading>We are part of</Heading>
-        <StyleCard key={index} className="box">
+
+        <Box key={index} onMouseEnter={hoverIn} onMouseLeave={hoverOut}>
+          <Card.Title
+            style={{
+              marginTop:'2%',
+              textShadow: "0px 4px 10px rgba(0,0,0,0.5)",
+              letterSpacing:'2px',
+              fontSize: "1.5rem",
+              visibility: set ? "hidden" : "visible",
+            }}
+          >
+            {card.name}
+          </Card.Title>
+
+          <Image variant="top" src={card.logo} />
+
+          <a href={card.url}>
+            <CardBody style={{ left: set ? "0%" : "-100%" }}>
+              <Card.Title style={{ fontWeight: "bold", fontSize: "2rem" }}>
+                {card.name}
+              </Card.Title>
+              <Button
+                  style={{
+                    justifyContent: "center",
+                    margin: "auto",
+                    color: "#000",
+                    backgroundColor: "#fff",
+                    border: "3px solid #000",
+                    borderOutline: "#000",
+                  }}
+                  src={card.url}
+                >
+                  Website
+                </Button>
+              
+            </CardBody>
+          </a>
           <Wave
-            fill="url(#gradient)"
+            fill="#0069D9"
             paused={false}
             options={{
               height: 20,
@@ -76,31 +119,11 @@ const OpenSourceProgramme = () => {
               speed: 0.5,
               points: 3,
             }}
-          >
-            <defs>
-              <linearGradient id="gradient" gradientTransform="rotate(90)">
-                <stop offset="10%" stopColor="#0F2027" />
-                <stop offset="90%" stopColor="#203A43" />
-              </linearGradient>
-            </defs>
-          </Wave>
-
-          <Card.Title>
-            <StyleImg variant="top" src={card.logo} />
-          </Card.Title>
-
-          <Card.Body
-            style={{ background: "linear-gradient(#0F2027,#203A43,#2C5364)" }}
-          >
-            <Card.Title style={{ color: "white" }}>{card.name}</Card.Title>
-            <a href={card.url}>
-              <Button2 src={card.url}>Website</Button2>
-            </a>
-          </Card.Body>
-        </StyleCard>
+          />
+        </Box>
       </div>
-    );
-  };
-  return <Grid>{cardInfo.map(renderCard)}</Grid>;
+    )
+  }
+  return <Grid>{cardInfo.map(renderCard)}</Grid>
 };
 export default OpenSourceProgramme;
