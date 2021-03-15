768fd49692a34eb143796af13cd8c6a8e13576c1
Prajwal-ctrl <prajwalsontakke2001@gmail.com>
2021-03-15 23:10:34 +0530

    final commit :

--- hotelontouch/src/pages/HomePage.js
+++ hotelontouch/src/pages/HomePage.js
@@ -1,46 +1,18 @@
-
-import React, {useState} from 'react';
-import { Container, Jumbotron, Button } from 'react-bootstrap';
+import React from 'react';
+import { Container } from 'react-bootstrap';
 import { AdminCard ,OpenSourceProgramme,AllContributors} from '../components/index';
-import { projectRepository } from '../data/projectData';
-import {lightTheme , darkTheme} from "./Theme" ;
-import {ThemeProvider} from "styled-components";
 import SystemFeature from '../components/SystemFeature';
 
-
 function HomePage() {
-
-  const [theme] = useState('light');
   return (
-    <ThemeProvider theme={theme === 'light'? lightTheme: darkTheme}>
     <Container>
       <br />
       <br />
-      <Jumbotron>
-        <h1>Welcome to project HotelOnTouch</h1>
-        <p>
-          In Todays Generation the Hotel Industries are changing rapidly so this
-          platform will manage the in house needs of hotels like laundry
-          service, Room Service Management, Bar, Restraunt Order
-          Management.There will be one customer end system and hotel end system
-          and customer can request hotel for one of the above service. We will
-          use the basic ReactJS, MongoDB and ExpressJS for the web development
-          purpose and the entire project will be open source and free to use for
-          the end users. This project aims to give a hands on experience to the
-          beginners a hand on experience in the open source journey
-        </p>
-        <p>
-          <Button variant='primary' href={projectRepository}>
-            Contribue to this Project
-          </Button>
-        </p>
-      </Jumbotron>
       <SystemFeature />
       <AdminCard />
       <OpenSourceProgramme/>
       <AllContributors />
     </Container>
-  </ThemeProvider>
   );
 }
 
