7ed72f35b30f638f1671da106ee453a03327452f
Aniket Singh <thor98571@gmail.com>
2021-03-16 11:27:45 +0530

    Update HomePage.js

--- hotelontouch/src/pages/HomePage.js
+++ hotelontouch/src/pages/HomePage.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Container, Jumbotron, Button } from 'react-bootstrap';
 import { AdminCard ,OpenSourceProgramme,AllContributors,OurSolution} from '../components/index';
 import { projectRepository } from '../data/projectData';
-import { Container } from 'react-bootstrap';
-import { AdminCard ,OpenSourceProgramme,AllContributors} from '../components/index';
 import SystemFeature from '../components/SystemFeature';
 function HomePage() {
   return (
