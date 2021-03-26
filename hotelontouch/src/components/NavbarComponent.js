2376fead8bf20b1b4938a2c54c254b0a8a7ca1a8
shawavisek35 <shawavisek35@gmail.com>
2021-03-26 19:13:44 +0530

    a little modification

--- hotelontouch/src/components/NavbarComponent.js
+++ hotelontouch/src/components/NavbarComponent.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { faBuilding } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   Navbar,
   Nav,
