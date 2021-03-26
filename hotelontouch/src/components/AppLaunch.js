131337b11fdab5296ac4ad39c7001d074fe6dbb6
abhishek7997 <68701271+abhishek7997@users.noreply.github.com>
2021-03-26 14:01:16 +0530

    Fixed theme button now working issue

--- hotelontouch/src/components/AppLaunch.js
+++ hotelontouch/src/components/AppLaunch.js
@@ -15,12 +15,21 @@ line-height:200%;
 padding-bottom:40px;
 `
 
+const Wrapper = styled.div`
+    width: 1280px;
+    background: #c9cfdf;
+    padding: 50px;
+    margin: 60px auto;
+    border-radius: 10px;
+    position: relative;
+`
+
 const desc = "In Todays Generation the Hotel Industries are changing rapidly so this platform will manage the in house needs of hotels like laundry service, Room Service Management, Bar, Restraunt Order Management.There will be one customer end system and hotel end system and customer can request hotel for one of the above service.";
 const feat = "In Todays Generation the Hotel Industries are changing rapidly so this platform will manage the in house needs of hotels like laundry service, Room Service Management, Bar, Restraunt Order Management.There will be one customer end system and hotel end system and customer can request hotel for one of the above service.";
 
-
 function AppLaunch() {
     return (
+        <Wrapper>
         <Jumbotron>
             <h1 style={{ marginBottom: '4rem', fontSize:'250%', letterSpacing:'0.3rem' }} className="d-flex flex-column justify-content-center align-items-center">Welcome to HotelOnTouch!</h1>
             <Container fluid >
@@ -35,6 +44,7 @@ function AppLaunch() {
                 </Row>
             </Container>
         </Jumbotron>
+        </Wrapper>
     );
 }
 
