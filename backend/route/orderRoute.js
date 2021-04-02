3e48f856ae7a890829cf2a5f7a4f0919a2f35e91
Sukriti-sood <soodsukriti1111@gmail.com>
2021-04-02 07:14:29 +0530

    Small fix

--- backend/route/orderRoute.js
+++ backend/route/orderRoute.js
@@ -2,49 +2,18 @@ const { Router } = require("express");
 const router = Router();
 const OrderController = require("../controller/orderController");
 
-/**
- * @swagger
- * /sampleroute:
- *   get:
- *     summary: Test Route 1
- *     description: This is a testing route to check the development server setup
-*/
 router.get("/", OrderController.allorders);
 
-/**
- * @swagger
- * /sampleroute/:id:
- *   get:
- *     summary: Test Route 2
- *     description: This is a testing route to check the development server setup
-*/
+
 router.get('/:customerId', OrderController.getOrderbyCustomer);
 
-/**
- * @swagger
- * /sampleroute:
- *   post:
- *     summary: Test Route 3
- *     description: This is a testing route to check the development server setup
-*/
+
 router.post('/',OrderController. createOrder);
 
-/**
- * @swagger
- * /sampleroute/:id:
- *   put:
- *     summary: Test Route 4
- *     description: This is a testing route to check the development server setup
-*/
+
 router.put('/:id',OrderController.updateOrder);
 
-/**
- * @swagger
- * /sampleroute/:id:
- *   delete:
- *     summary: Test Route 5
- *     description: This is a testing route to check the development server setup
-*/
+
 router.delete('/:id',OrderController. deleteOrder);
 
 module.exports = router;    
