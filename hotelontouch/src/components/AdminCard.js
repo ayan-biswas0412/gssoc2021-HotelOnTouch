30c0faae79ebcdbf034cefe898317aa9f7efa453
Ankita <ankitapal7777@gmail.com>
2021-04-04 14:12:25 +0530

    Admin Card

--- hotelontouch/src/components/AdminCard.js
+++ hotelontouch/src/components/AdminCard.js
@@ -1,119 +1,170 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import styled from 'styled-components';
-const StyledCardContainer = styled.div`
+import React, { useState } from "react";
+import { Card, Button } from "react-bootstrap";
+import styled from "styled-components";
+import Wave from "react-wavify";
+
+const Heading = styled.h1`
+  font-size: 4rem;
+  font-family: "sans-serif";
+  margin-top: 2rem;
+  margin-bottom: 1.5rem;
+  letter-spacing: 3px;
+  color: #333;
+  -webkit-text-stroke: 1px #282828;
+  text-shadow: 0px 4px 4px #282828;
+`;
+const Grid = styled.div`
   display: flex;
-  justify-content: center;
-`;
-
-const StyledCard = styled(Card)`
-  width: 16rem;
-  border-radius: 0.8em !important;
-  opacity: 1;
-  animation: fadeIn ease 1s;
-  -webkit-animation: fadeIn ease 1s;
-  padding: 0.4%;
-
-  @keyframes fadeIn {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-
-  @-moz-keyframes fadeIn {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-
-  @-webkit-keyframes fadeIn {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-
-  @-o-keyframes fadeIn {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-
-  @-ms-keyframes fadeIn {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-`;
-
-const StyledSocial = styled.div`
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
   text-align: center;
-  margin-bottom: 0.8em;
 `;
 
-const StyledImage = styled(Card.Img)`
-  border-radius: 0.8em !important;
+const CardBody = styled(Card.Body)`
+  background: #0069d9;
+  opacity: 0.9;
+  position: absolute;
+  top: 0%;
+  right: -100%;
+  height: 100%;
+  width: 100%;
+  color: #fff;
+  padding: 20px;
+  display: grid;
+  place-items: center;
+  align-items: center;
+  box-sizing: border-box;
+  transition: all 0.5s;
 `;
 
-const AdminCard = ({
-  projectAdminName = 'Ayan Biswas',
-  GitHubUserName = 'ayan-biswas0412',
-  webSite = 'https://ayanbiswas.in',
-}) => {
-  return (
-    <StyledCardContainer>
-      <StyledCard border='primary'>
-        <StyledImage
-          variant='top'
-          src={`https://avatars.githubusercontent.com/${GitHubUserName}`}
-        />
-        <Card.Body>
-          <Card.Title style={{ textAlign: 'center' }}>
-            {projectAdminName}
+const Box = styled.div`
+  width: 17rem;
+  height: 23rem;
+  position: relative;
+  margin: 10px auto;
+  overflow: hidden;
+  box-shadow: 0 0 20px 2px rgba(0, 0, 0, 0.5);
+`;
+
+const Image = styled(Card.Img)`
+  width: 100%;
+  height: auto;
+  overflow: hidden;
+`;
+
+const AdminCard = () => {
+  const [set, setState] = useState(false);
+  const cardInfo = [
+    {
+      projectAdminName: "Ayan Biswas",
+      GitHubUserName: "ayan-biswas0412",
+      webSite: "https://ayanbiswas.in",
+    },
+  ];
+  function hoverIn() {
+    setState(true);
+  }
+  function hoverOut() {
+    setState(false);
+  }
+  const renderCard = (card, index) => {
+    return (
+      <div>
+        <Heading>Project Admin</Heading>
+
+        <Box key={index} onMouseEnter={hoverIn} onMouseLeave={hoverOut}>
+          <Card.Title
+            style={{
+              marginTop: "2%",
+              textShadow: "0px 4px 10px rgba(0,0,0,0.5)",
+              color: "black",
+              letterSpacing: "2px",
+              fontSize: "2rem",
+              margin: "0%",
+              visibility: set ? "hidden" : "visible",
+              backgroundColor: "lightblue",
+              padding: "5px",
+            }}
+          >
+            {card.projectAdminName}
           </Card.Title>
-          <StyledSocial>
-            <a
-              href={`https://github.com/${GitHubUserName}`}
-              rel='noopener noreferrer'
-              target='_blank'
-            >
-              <img
-                src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'
-                alt='GitHub Profile'
-                height='30'
-                width='30'
-              />
-              {GitHubUserName}
-            </a>
-          </StyledSocial>
-          <StyledSocial>
-            <a href={webSite} rel='noopener noreferrer' target='_blank'>
-              <img
-                src='https://www.saganetwork.net/img/domainikon.png'
-                alt='WebSite Logo'
-                height='30'
-                width='30'
-              />
-            </a>
-          </StyledSocial>
-        </Card.Body>
-      </StyledCard>
-    </StyledCardContainer>
-  );
-};
 
+          <Image
+            variant="top"
+            src={`https://avatars.githubusercontent.com/${card.GitHubUserName}`}
+          />
+
+          <a href={`https://github.com/${card.GitHubUserName}`}>
+            <CardBody style={{ right: set ? "0%" : "-100%" }}>
+              <Card.Title style={{ fontWeight: "bold", fontSize: "2rem" }}>
+                {card.projectAdminName}
+              </Card.Title>
+              <Button
+                style={{
+                  justifyContent: "center",
+                  margin: "auto",
+                  marginBottom: "5px",
+                  color: "#000",
+                  backgroundColor: "#fff",
+                  border: "3px solid #000",
+                  borderOutline: "#000",
+                  borderRadius: "1rem",
+                }}
+                src={card.url}
+              >
+                <a
+                  href={card.webSite}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <img
+                    src="https://www.saganetwork.net/img/domainikon.png"
+                    alt="WebSite Logo"
+                    height="30"
+                    width="30"
+                  />{" "}
+                </a>
+              </Button>
+              <Button
+                style={{
+                  justifyContent: "center",
+                  margin: "auto",
+                  marginBottom: "16px",
+
+                  color: "#000",
+                  backgroundColor: "#fff",
+                  border: "3px solid #000",
+                  borderOutline: "#000",
+                  borderRadius: "1rem",
+                }}
+                src={card.url}
+              >
+                <img
+                  src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                  alt="GitHub Profile"
+                  height="30"
+                  width="30"
+                />
+                ayan-biswas0142
+              </Button>
+            </CardBody>
+          </a>
+          <Wave
+            fill="#0069D9"
+            paused={false}
+            options={{
+              height: 20,
+              amplitude: 20,
+              speed: 0.5,
+              points: 3,
+            }}
+          />
+        </Box>
+      </div>
+    );
+  };
+  return <Grid>{cardInfo.map(renderCard)}</Grid>;
+};
 export default AdminCard;
